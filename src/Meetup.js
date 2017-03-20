927457f0b7d8da58308c40ff46e0153ef879f295
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2017-03-20 10:28:46 +0100

    Improve Meetup wrapper class.

--- src/Meetup.js
+++ src/Meetup.js
@@ -1,68 +1,47 @@
 
 import MeetupAPI from 'meetup-api';
 
+const promisify = [
+  'getEvents',
+];
+
 export default class Meetup {
   constructor(options) {
     this.api = MeetupAPI(options);
-  }
 
-  getRecentEvent(group) {
-    return new Promise((resolve, reject) => {
-      this.api.getEvents({
-        group_urlname: group,
-        scroll: 'recent_past',
-        status: 'past',
-        desc: true,
-        text_format: 'plain',
-      }, (err, response) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-
-        if (!response.results || !response.results.length) {
-          resolve(null);
-          return;
-        }
-
-        resolve(this.wrapEvent(response.results[0]));
+    promisify.forEach((method) => {
+      this[method] = params => new Promise((resolve, reject) => {
+        this.api[method](params, (err, response) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(response);
+          }
+        });
       });
     });
+  }
+
+  getNearbyEvents() {
+    return this.findEvents({
+      text_format: 'plain',
+    }).then(response => response.results);
+  }
+
+  getPastEvents(group) {
+    return this.getEvents({
+      group_urlname: group,
+      status: 'past',
+      desc: true,
+      text_format: 'plain',
+    }).then(response => response.results);
   }
 
   getUpcomingEvent(group) {
-    return new Promise((resolve, reject) => {
-      this.api.getEvents({
-        group_urlname: group,
-        scroll: 'next_upcoming',
-        status: 'upcoming',
-        text_format: 'plain',
-      }, (err, response) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-
-        if (!response.results || !response.results.length) {
-          resolve(null);
-          return;
-        }
-
-        resolve(this.wrapEvent(response.results[0]));
-      });
-    });
-  }
-
-  wrapEvent(event) {
-    return {
-      id: event.id,
-      status: event.status,
-      title: event.name,
-      date: new Date(event.time + event.utc_offset),
-      group: event.group,
-      venue: event.venue,
-      visibility: event.visibility,
-      announced: event.announced,
-    };
+    return this.getEvents({
+      group_urlname: group,
+      status: 'upcoming',
+      text_format: 'plain',
+    }).then(response => response.results);
   }
 }
