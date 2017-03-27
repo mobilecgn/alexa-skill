dcdb96f87eee309664b6e2ca8b8535fe5a049bad
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2017-03-27 03:32:26 +0200

    Improve utterances.

--- src/intents.js
+++ src/intents.js
@@ -3,15 +3,15 @@ export default {
   LastMeetup: {
     utterances: [
       '{wann|wo} fand das {letzte|vorherige} {Event|Meetup} statt',
+      '{wann|wo} war das {letzte|vorherige} {Event|Meetup}',
       '{wann|wo} das {letzte|vorherige} {Event|Meetup} war',
-      'das {letzte|vorherige} {Event|Meetup}',
       'das {letzte|vorherige} {Event|Meetup}',
     ],
   },
   NextMeetup: {
     utterances: [
-      '{wann|wo} ist das {nächste|kommende} {Event|Meetup}',
       '{wann|wo} findet das {nächste|kommende} {Event|Meetup} statt',
+      '{wann|wo} ist das {nächste|kommende} {Event|Meetup}',
       '{wann|wo} das {nächste|kommende} {Event|Meetup} {ist|stattfindet}',
       '{das|mir ein} {nächste|kommende} {Event|Meetup}',
       'nach dem {nächstem|kommendem} {Event|Meetup}',
